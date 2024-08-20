fa3974be81e22345932db39e66a1a365bb27277f
Martin Hernandez <martinalejandrohernandez1@gmail.com>
2024-08-20 20:34:31 -0300

    se agrego bootstrap

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="http://localhost/PWD/assets/css/bootstrap.min.css">
+    <title>PWD</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">TP-1</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">TP-2</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">TP-3</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+    <h1>Trabajo Practico PWD</h1>
+
+    <script src="http://localhost/PWD/assets/js/bootstrap.min.js"></script>
+</body>
+</html>

e20b09b2c40199273aacf337d0220beb7b37d9e4
Martin Hernandez <martinalejandrohernandez1@gmail.com>
2024-08-21 10:41:40 -0300

    barra de navegación

--- index.php
+++ index.php
@@ -1,32 +1,62 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="http://localhost/PWD/assets/css/bootstrap.min.css">
-    <title>PWD</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <link rel="stylesheet" href="http://localhost/PWD/assets/css/bootstrap.min.css">
+  <title>PWD</title>
 </head>
+
 <body>
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
+  <header>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary navbar bg-dark border-bottom border-body" data-bs-theme="dark"">
+      <div class=" container-fluid">
+      <a class="navbar-brand" href="#">P.W.D</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Trabajo Práctico 1
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Ejercicio 1</a></li>
+              <li><a class="dropdown-item" href="#">Ejercicio 2</a></li>
+              <li><a class="dropdown-item" href="#">Ejercicio 3</a></li>
+            </ul>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Trabajo Práctico 2
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Ejercicio 1</a></li>
+              <li><a class="dropdown-item" href="#">Ejercicio 2</a></li>
+              <li><a class="dropdown-item" href="#">Ejercicio 3</a></li>
+            </ul>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Trabajo Práctico 3
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Ejercicio 1</a></li>
+              <li><a class="dropdown-item" href="#">Ejercicio 2</a></li>
+              <li><a class="dropdown-item" href="#">Ejercicio 3</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+      </div>
+    </nav>
+  </header>
 
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">TP-1</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">TP-2</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">TP-3</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-    <h1>Trabajo Practico PWD</h1>
-
-    <script src="http://localhost/PWD/assets/js/bootstrap.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
+
 </html>

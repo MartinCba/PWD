51467e8ee79b1fb19e6e2f739c8b19e243d511be
Martin Hernandez <martinalejandrohernandez1@gmail.com>
2024-08-21 12:29:47 -0300

    iniciando tp 1

---
+++ views/structure.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>PWD</title>
+</head>
+
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary navbar bg-dark border-bottom border-body" data-bs-theme="dark"">
+      <div class=" container-fluid">
+            <a class="navbar-brand" href="#">P.W.D</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Trabajo Práctico 1
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Ejercicio 1</a></li>
+                            <li><a class="dropdown-item" href="#">Ejercicio 2</a></li>
+                            <li><a class="dropdown-item" href="#">Ejercicio 3</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Trabajo Práctico 2
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Ejercicio 1</a></li>
+                            <li><a class="dropdown-item" href="#">Ejercicio 2</a></li>
+                            <li><a class="dropdown-item" href="#">Ejercicio 3</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Trabajo Práctico 3
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Ejercicio 1</a></li>
+                            <li><a class="dropdown-item" href="#">Ejercicio 2</a></li>
+                            <li><a class="dropdown-item" href="#">Ejercicio 3</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+            </div>
+        </nav>
+    </header>
+
+    <footer>
+        <nav class="navbar fixed-bottom bg-body-tertiary bg-dark border-bottom border-body" data-bs-theme="dark"">
+      <div class=" container-fluid">
+            <span class="navbar-text">
+                MARTÍN HERNANDEZ
+            </span>
+            <span class="navbar-text">
+                PROGRAMACIÓN WEB DINÁMICA
+            </span>
+            </div>
+        </nav>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+
+</html>

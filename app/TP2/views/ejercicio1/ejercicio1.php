f0314b31dcadf5f18b631bd84bc0ba1c789b495c
Martin Hernandez <martinalejandrohernandez1@gmail.com>
2024-08-22 20:16:01 -0300

    organizacion

---
+++ app/TP2/views/ejercicio1/ejercicio1.php
@@ -0,0 +1,20 @@
+<?php
+include_once("../assets/structure.php");
+?>
+<div class="container card-container d-flex justify-content-center align-items-center" style="height: 80vh">
+    <div class="card text-center bg-dark text-light" style="width: 28rem;">
+        <div class="card-header">
+            <h3>Ejercicio 1</h3>
+            <p>Se investigó y se implementarán validaciones con JQuery.</p>
+        </div>
+        <div class="card-body">
+            
+        </div>
+    </div>
+</div>
+
+<!-- 
+    tp2 ejercicio 1
+    Investigue y pruebe la validación de formularios usando alguna librería o framework
+    javaScript (JQuery, Mootools, Dojo, Prototype, etc).
+-->

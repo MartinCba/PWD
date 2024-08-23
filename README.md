094c9a95dfaa660fb8e1527cd607b06279c7efd9
Martin Hernandez <martinalejandrohernandez1@gmail.com>
2024-08-23 18:35:35 -0300

    README

---
+++ README.md
@@ -0,0 +1,12 @@
+
+# PROGRAMACION WEB DINAMICA.
+
+Proyecto para la materia PWD, entrega de Trabajos practicos 1,2 y 3 desarrolladas en grupo.
+
+
+## Integrantes
+
+- [@MartinCba](https://github.com/MartinCba)
+- [@GermanMetzger](https://github.com/GermanMetzger)
+- [@Martin-VillegasReibold](https://github.com/Martin-VillegasReibold)
+
--- app/TP3/views/assets/js/jquery1.js
+++ app/TP3/views/assets/js/jquery1.js
@@ -1,26 +1,38 @@
 $(document).ready(function () {
   $("form").on("submit", function (event) {
     let isValid = true;
-    let fileInput = $("#archivo");
-    let filePath = fileInput.val();
+    let fileInput = $("#archivo")[0];
+    let filePath = fileInput.value;
     let allowedExtensions = /(\.doc|\.pdf)$/i;
+    let maxSize = 2 * 1024 * 1024; // 2MB en bytes
 
     // Validar que se haya seleccionado un archivo
     if (filePath === "") {
-      fileInput.addClass("is-invalid");
+      $("#archivo").addClass("is-invalid");
       isValid = false;
     } else {
-      fileInput.removeClass("is-invalid");
+      $("#archivo").removeClass("is-invalid");
 
       // Validar extensión del archivo
       if (!allowedExtensions.exec(filePath)) {
-        fileInput.addClass("is-invalid");
-        fileInput
+        $("#archivo").addClass("is-invalid");
+        $("#archivo")
           .next(".invalid-feedback")
           .text("Solo se permiten archivos .doc o .pdf.");
         isValid = false;
       } else {
-        fileInput.removeClass("is-invalid");
+        $("#archivo").removeClass("is-invalid");
+
+        // Validar tamaño del archivo
+        if (fileInput.files[0].size > maxSize) {
+          $("#archivo").addClass("is-invalid");
+          $("#archivo")
+            .next(".invalid-feedback")
+            .text("El archivo no puede pesar más de 2 MB.");
+          isValid = false;
+        } else {
+          $("#archivo").removeClass("is-invalid");
+        }
       }
     }
 

8d3611809bf2eb64bd63e80eb455164b0004d0b8
Martin Hernandez <martinalejandrohernandez1@gmail.com>
2024-09-09 10:35:21 -0300

    inicio TP4

---
+++ app/TP4/views/assets/structure/header.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary navbar bg-dark border-bottom border-body" data-bs-theme="dark"">
+      <div class=" container-fluid">
+            <a class="navbar-brand" href="../../../../index.php">P.W.D</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Trabajo Práctico 1
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="../../../../app/TP1/views/ejercicio1/ejercicio1.php">Ejercicio 1</a></li>
+                            <li><a class="dropdown-item" href="../../../../app/TP1/views/ejercicio2/ejercicio2.php">Ejercicio 2</a></li>
+                            <li><a class="dropdown-item" href="../../../../app/TP1/views/ejercicio3/ejercicio3.php">Ejercicio 3</a></li>
+                            <li><a class="dropdown-item" href="../../../../app/TP1/views/ejercicio4/ejercicio4.php">Ejercicio 4</a></li>
+                            <li><a class="dropdown-item" href="../../../../app/TP1/views/ejercicio5/ejercicio5.php">Ejercicio 5</a></li>
+                            <li><a class="dropdown-item" href="../../../../app/TP1/views/ejercicio6/ejercicio6.php">Ejercicio 6</a></li>
+                            <li><a class="dropdown-item" href="../../../../app/TP1/views/ejercicio7/ejercicio7.php">Ejercicio 7</a></li>
+                            <li><a class="dropdown-item" href="../../../../app/TP1/views/ejercicio8/ejercicio8.php">Ejercicio 8</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Trabajo Práctico 2
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="../../../../app/TP2/views/ejercicio1/ejercicio1.php">Ejercicio 1</a></li>
+                            <li><a class="dropdown-item" href="../../../../app/TP2/views/ejercicio2-1/ejercicio1.php">Ejercicio 2-1</a></li>
+                            <li><a class="dropdown-item" href="../../../../app/TP2/views/ejercicio2-2/ejercicio2.php">Ejercicio 2-2</a></li>
+                            <li><a class="dropdown-item" href="../../../../app/TP2/views/ejercicio2-3/ejercicio3.php">Ejercicio 2-3</a></li>
+                            <li><a class="dropdown-item" href="../../../../app/TP2/views/ejercicio2-4/ejercicio4.php">Ejercicio 2-4</a></li>
+                            <li><a class="dropdown-item" href="../../../../app/TP2/views/ejercicio2-5/ejercicio5.php">Ejercicio 2-5</a></li>
+                            <li><a class="dropdown-item" href="../../../../app/TP2/views/ejercicio2-6/ejercicio6.php">Ejercicio 2-6</a></li>
+                            <li><a class="dropdown-item" href="../../../../app/TP2/views/ejercicio2-7/ejercicio7.php">Ejercicio 2-7</a></li>
+                            <li><a class="dropdown-item" href="../../../../app/TP2/views/ejercicio2-8/ejercicio8.php">Ejercicio 2-8</a></li>
+                            <li><a class="dropdown-item" href="../../../../app/TP2/views/ejercicio3/ejercicio3.php">Ejercicio 3</a></li>
+                            <li><a class="dropdown-item" href="../../../../app/TP2/views/ejercicio4/ejercicio4.php">Ejercicio 4</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Trabajo Práctico 3
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="../../../../app/TP3/views/ejercicio1/ejercicio1.php">Ejercicio 1</a></li>
+                            <li><a class="dropdown-item" href="../../../../app/TP3/views/ejercicio2/ejercicio2.php">Ejercicio 2</a></li>
+                            <li><a class="dropdown-item" href="../../../../app/TP3/views/ejercicio3/ejercicio3.php">Ejercicio 3</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+            </div>
+        </nav>
+    </header>
+</body>
+</html>
